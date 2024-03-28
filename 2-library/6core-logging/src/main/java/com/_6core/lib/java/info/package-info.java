f013f2654f7cee37e86db034d1acc3981d509cbf
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-28 22:44:24 +0100

    [6CORE-007] delete package-info

--- 2-library/6core-logging/src/main/java/com/_6core/lib/java/info/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides logging configuration for microservices.
- *
- * <p>This package contains the configuration file for logging in microservices. The {@code
- * logback.xml} file specifies the logging settings and behavior for the application.
- */
-package com._6core.lib.java.info;
