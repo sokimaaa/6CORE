d6f1248a65642f5e9f7de686f7226a64e6ce1efc
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-03-28 21:54:15 +0100

    [6CORE-007] corrected pom because of version error

--- 2-library/6core-logging/src/main/java/com/_6core/lib/java/info/package-info.java
+++ 2-library/6core-logging/src/main/java/com/_6core/lib/java/info/package-info.java
@@ -1,9 +1,7 @@
 /**
  * Provides logging configuration for microservices.
- * <p>
- * This package contains the configuration file for logging in microservices.
- * The {@code logback.xml} file specifies the logging settings and behavior
- * for the application.
  *
+ * <p>This package contains the configuration file for logging in microservices. The {@code
+ * logback.xml} file specifies the logging settings and behavior for the application.
  */
-package com._6core.lib.java.logging;
+package com._6core.lib.java.info;
