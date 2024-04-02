8d27b3de1ea52c7bd8b17965322e2055bbe9a970
Yuriy.Khilko <yurakhilko@gmail.com>
2024-04-02 16:34:34 +0300

    [6CORE-043] fixes after review.

---
+++ 3-platform/warehouse/db/postgres/02-create-products-table.sql
@@ -0,0 +1,32 @@
+-- products table
+
+CREATE TABLE IF NOT EXISTS public.products
+(
+    id          varchar NOT NULL,
+    "name"      varchar NOT NULL,
+    description varchar NULL,
+    image       varchar NULL,
+    price       bigint  NOT NULL,
+    category    varchar NULL,
+    CONSTRAINT products_pk PRIMARY KEY (id)
+);
+
+-- Column checks
+
+ALTER TABLE public.products
+    ADD CONSTRAINT products_price_check CHECK (price >= 0);
+
+-- Column comments
+
+COMMENT
+    ON COLUMN public.products.id IS 'PK of product record';
+COMMENT
+    ON COLUMN public.products."name" IS 'product name';
+COMMENT
+    ON COLUMN public.products.description IS 'product description';
+COMMENT
+    ON COLUMN public.products.image IS 'product image location path';
+COMMENT
+    ON COLUMN public.products.price IS 'price of unit of product';
+COMMENT
+    ON COLUMN public.products.category IS 'category of product';
