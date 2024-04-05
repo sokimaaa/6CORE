6c09a278270d464dfff65578b3297a41cb438458
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-04-05 13:09:01 +0200

    return properties to pom; added package-info

---
+++ 2-library/utils-logging/src/main/java/com/_6core/lib/java/utils/logging/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>This package contains the configuration file for logging in microservices.
+ * The {@code logback.xml} file specifies the logging settings and behavior for the application.
+ */
+
+@Deprecated
+package com._6core.lib.java.utils.logging;
