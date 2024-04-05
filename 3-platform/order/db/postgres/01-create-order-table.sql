d9a6e26b1ca74e474c0dcfd27a41ab93ba472c42
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-05 13:54:17 +0300

    [6CORE-042] add ER diagram and SQL Table for Order Service

---
+++ 3-platform/order/db/postgres/01-create-order-table.sql
@@ -0,0 +1,25 @@
+-- orders table
+
+CREATE TABLE IF NOT EXISTS public.orders (
+    id          varchar     NOT NULL,
+    created_at  timestamp   NOT NULL,
+    total       bigint      NOT NULL,
+    status      varchar     NULL,
+    CONSTRAINT orders_pk PRIMARY KEY (id)
+);
+
+-- Column checks
+
+ALTER TABLE public.orders
+    ADD CONSTRAINT orders_total_check CHECK (total >= 0);
+
+-- Column comments
+
+COMMENT
+    ON COLUMN public.orders.id IS 'PK of order record';
+COMMENT
+    ON COLUMN public.orders.created_at IS 'date and time of order creation';
+COMMENT
+    ON COLUMN public.orders.total IS 'total price of the order';
+COMMENT
+    ON COLUMN public.orders.status IS 'order current status';
