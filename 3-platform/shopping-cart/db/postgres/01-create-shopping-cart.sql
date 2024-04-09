bc0a5925fdf0c271148e78b775f5516364f31c17
ANDREY <andrey.kiyas@gmail.com>
2024-04-09 13:36:54 +0300

    [6CORE-041] Fixed table script and ER diagram after second code review

--- 3-platform/shopping-cart/db/postgres/01-create-shopping-cart.sql
+++ 3-platform/shopping-cart/db/postgres/01-create-shopping-cart.sql
@@ -1,11 +1,8 @@
--- DROPPING TABLE IF EXISTS
--- DROP TABLE IF EXISTS shopping_carts;
-
 -- CREATING TABLE "shopping_carts"
 CREATE TABLE IF NOT EXISTS shopping_carts
 (
     id         VARCHAR(20) NOT NULL,
-    product_id VARCHAR(150)
+    product_id VARCHAR(150) NOT NULL
 );
 
 -- SET INDEXES WHERE NEEDED
@@ -15,5 +12,5 @@ CREATE INDEX idx_shopping_carts_id ON shopping_carts (id);
 COMMENT ON TABLE shopping_carts IS 'Table containing shopping cart information';
 
 -- SET COMMENT ON COLUMN
-COMMENT ON COLUMN shopping_carts.id IS 'The cart unique identifier';
+COMMENT ON COLUMN shopping_carts.id IS 'The cart identifier';
 COMMENT ON COLUMN shopping_carts.product_id IS 'A list of product ids that have been added to the shopping cart';
