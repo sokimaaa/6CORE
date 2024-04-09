5712a9b6dd4d3e62da1990f5380d7ae489696b97
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-04-09 14:37:52 +0200

    [6CORE-011] package info problem

--- 2-library/domain-model-6core-java/src/main/java/com/_6core/lib/java/domain/model/payment/package-info.java
+++ 2-library/domain-model-6core-java/src/main/java/com/_6core/lib/java/domain/model/payment/package-info.java
@@ -1,7 +1,4 @@
 /**
  * need to think about possible model
  */
-@Generated({})
 package com._6core.lib.java.domain.model.payment;
-
-import javax.annotation.processing.Generated;
--- 2-library/domain-model-6core-java/src/main/java/com/_6core/lib/java/domain/model/shipping/package-info.java
+++ 2-library/domain-model-6core-java/src/main/java/com/_6core/lib/java/domain/model/shipping/package-info.java
@@ -1,7 +1,4 @@
 /**
  * need to think about possible model
  */
-@Generated({})
 package com._6core.lib.java.domain.model.shipping;
-
-import javax.annotation.processing.Generated;
