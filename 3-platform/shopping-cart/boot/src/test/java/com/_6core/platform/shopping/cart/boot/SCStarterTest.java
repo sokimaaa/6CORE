b51dbc45be0977b5d726462e6305081f6c85323d
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-15 20:03:56 +0300

    [6CORE-107] add context load tests to all services

---
+++ 3-platform/shopping-cart/boot/src/test/java/com/_6core/platform/shopping/cart/boot/SCStarterTest.java
@@ -0,0 +1,10 @@
+package com._6core.platform.shopping.cart.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SCStarterTest {
+  @Test
+  void contextLoads() {}
+}
