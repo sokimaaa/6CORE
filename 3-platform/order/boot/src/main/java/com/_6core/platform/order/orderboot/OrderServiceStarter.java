dec3c79239f11cb40e63a670feb8615e9c2ea022
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-18 15:43:55 +0300

    [6CORE-076] fixed db properties setup

--- 3-platform/order/boot/src/main/java/com/_6core/platform/order/orderboot/OrderServiceStarter.java
+++ 3-platform/order/boot/src/main/java/com/_6core/platform/order/orderboot/OrderServiceStarter.java
@@ -2,18 +2,9 @@ package com._6core.platform.order.orderboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {
-        R2dbcAutoConfiguration.class,
-        R2dbcDataAutoConfiguration.class,
-        R2dbcRepositoriesAutoConfiguration.class,
-        R2dbcTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication
 @ComponentScan("com._6core.platform.order")
 public class OrderServiceStarter {
 
