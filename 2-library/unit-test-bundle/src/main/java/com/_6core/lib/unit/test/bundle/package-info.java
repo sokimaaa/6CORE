72f74ebd30c90fa8aeba4676773cd7a1f1446ebd
ANDREY <andrey.kiyas@gmail.com>
2024-04-20 18:51:02 +0300

    [6CORE-065] Fixed pom.xlm files of domain modules, added `package-info.java` file

---
+++ 2-library/unit-test-bundle/src/main/java/com/_6core/lib/unit/test/bundle/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * JUnit and Mockito have many dependencies that take up
+ * a lot of space and create a messy dependency block in pom.xml
+ * This module contains one library that will have everything
+ * these dependencies to just inject just one
+ */
+
+package com._6core.lib.unit.test.bundle;
--- 2-library/unit-testing/src/main/java/com/_6core/lib/unit/testing/UnitTestingAutoConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com._6core.lib.unit.testing;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-
-@AutoConfiguration
-public class UnitTestingAutoConfiguration {
-}
