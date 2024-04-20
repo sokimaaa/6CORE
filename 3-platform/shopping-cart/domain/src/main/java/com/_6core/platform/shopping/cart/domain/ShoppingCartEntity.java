4619eb43f3a70987ef901236b914d86735828611
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-20 13:58:20 +0300

    [6CORE-088] add interface for communication with Database

---
+++ 3-platform/shopping-cart/domain/src/main/java/com/_6core/platform/shopping/cart/domain/ShoppingCartEntity.java
@@ -0,0 +1,5 @@
+package com._6core.platform.shopping.cart.domain;
+
+public class ShoppingCartEntity {
+  // TODO: implement domain object entity
+}
---
+++ 3-platform/shopping-cart/infra/src/main/java/com/_6core/platform/shopping/cart/infra/adapter/driven/persistence/ShoppingCartRepository.java
@@ -0,0 +1,10 @@
+package com._6core.platform.shopping.cart.infra.adapter.driven.persistence;
+
+import com._6core.platform.shopping.cart.domain.ShoppingCartEntity;
+import reactor.core.publisher.Mono;
+
+public interface ShoppingCartRepository {
+  Mono<ShoppingCartEntity> getCart(String cartId);
+
+  Mono<Void> cleanCart(String cartId);
+}
