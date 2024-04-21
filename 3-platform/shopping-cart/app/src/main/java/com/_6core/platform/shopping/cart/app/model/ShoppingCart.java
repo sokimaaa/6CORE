d18e6094cb36d8ce615e3bd79f890cec6ca9e552
ANDREY <andrey.kiyas@gmail.com>
2024-04-21 23:13:13 +0300

    [6CORE-077] Add reactive database support for Shopping Cart Service

---
+++ 3-platform/shopping-cart/app/src/main/java/com/_6core/platform/shopping/cart/app/model/ShoppingCart.java
@@ -0,0 +1,17 @@
+package com._6core.platform.shopping.cart.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    private String id;
+    private String[] productIds;
+}
