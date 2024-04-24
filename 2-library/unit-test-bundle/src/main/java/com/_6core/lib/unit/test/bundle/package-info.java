d29b170dca15c2569655168eb02bebf90b9d82da
ANDREY <andrey.kiyas@gmail.com>
2024-04-25 00:13:14 +0300

    [6CORE-077] Fixed package-info comments, maven-plugin, version to BOM, poms

--- 2-library/unit-test-bundle/src/main/java/com/_6core/lib/unit/test/bundle/package-info.java
+++ 2-library/unit-test-bundle/src/main/java/com/_6core/lib/unit/test/bundle/package-info.java
@@ -1,8 +1,1 @@
-/**
- * JUnit and Mockito have many dependencies that take up
- * a lot of space and create a messy dependency block in pom.xml
- * This module contains one library that will have everything
- * these dependencies to just inject just one
- */
-
 package com._6core.lib.unit.test.bundle;
