da9861bec77ac6b11679b37ad5f811ea8bf05069
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-01 16:49:51 +0300

    [6CORE-091] fix checkstyle

--- 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/driven/persistence/dto/ProductResponse.java
+++ 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/driven/persistence/dto/ProductResponse.java
@@ -2,8 +2,4 @@ package com._6core.platform.warehouseinfra.adapter.driven.persistence.dto;
 
 import java.math.BigDecimal;
 
-public record ProductResponse(String name,
-                              String description,
-                              String image,
-                              BigDecimal price) {
-}
+public record ProductResponse(String name, String description, String image, BigDecimal price) {}
