617a95fee9fd574c75e57a21886d2625507fb2fe
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-01 16:58:00 +0300

    [6CORE-091] fix checkstyle repository

--- 3-platform/warehouse/app/src/main/java/com/_6core/platform/warehouse/app/persistance/ProductRepository.java
+++ 3-platform/warehouse/app/src/main/java/com/_6core/platform/warehouse/app/persistance/ProductRepository.java
@@ -4,7 +4,6 @@ import com._6core.platform.warehouseinfra.adapter.driven.persistence.entity.Prod
 import java.util.List;
 import reactor.core.publisher.Flux;
 
-
 public interface ProductRepository {
   Flux<ProductEntity> getProductsByIds(List<String> productsIds);
 }
