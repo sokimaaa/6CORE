6d38559f3b6ab9918a29bc4089a592370a3900c0
ANDREY <andrey.kiyas@gmail.com>
2024-05-07 00:13:07 +0300

    [6CORE-077] Comments corrected, testing carried out

--- 3-platform/shopping-cart/infra/src/main/java/com/_6core/platform/shopping/cart/infra/model/ShoppingCart.java
+++ 3-platform/shopping-cart/infra/src/main/java/com/_6core/platform/shopping/cart/infra/model/ShoppingCart.java
@@ -1,16 +1,17 @@
-package com._6core.platform.shopping.cart.app.model;
+package com._6core.platform.shopping.cart.infra.model;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "shopping_carts")
 public class ShoppingCart {
-  @Id private String id;
-  private String[] productIds;
+  @Id private Long id;
+  private List<String> productIds;
 }
