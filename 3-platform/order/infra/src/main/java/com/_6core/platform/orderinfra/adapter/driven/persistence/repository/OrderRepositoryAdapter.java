1ce8a4bff37c07cbac068352f0567fe2bad75139
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-05-06 12:22:23 +0200

    [6CORE-089] delete repository adapter

--- 3-platform/order/infra/src/main/java/com/_6core/platform/orderinfra/adapter/driven/persistence/repository/OrderRepositoryAdapter.java
+++
@@ -1,8 +0,0 @@
-package com._6core.platform.orderinfra.adapter.driven.persistence.repository;
-
-import com._6core.platform.orderinfra.adapter.driven.persistence.entity.OrderEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepositoryAdapter extends ReactiveCrudRepository<OrderEntity, String> {}
