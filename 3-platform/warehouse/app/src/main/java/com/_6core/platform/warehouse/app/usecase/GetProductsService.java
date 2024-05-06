f6b8e25e3ae094c8c89449b75e14b34ca2e434b8
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-06 15:53:43 +0300

    [6CORE-091] fix checkstyle in GetProductsServiceTest

--- 3-platform/warehouse/app/src/main/java/com/_6core/platform/warehouse/app/usecase/GetProductsService.java
+++ 3-platform/warehouse/app/src/main/java/com/_6core/platform/warehouse/app/usecase/GetProductsService.java
@@ -2,8 +2,8 @@ package com._6core.platform.warehouse.app.usecase;
 
 import com._6core.lib.java.domain.model.warehouse.ProductV01;
 import com._6core.platform.warehouse.domain.dto.product.ProductsIds;
-import com._6core.platform.warehouse.domain.persistance.out.GetProductsPort;
 import com._6core.platform.warehouse.domain.persistance.in.GetProductsUseCase;
+import com._6core.platform.warehouse.domain.persistance.out.GetProductsPort;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
