7729dbfb1e3099a14039ecb68d803212d2972276
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-05-06 22:33:00 +0300

    [6CORE-102] add base implementation

--- 3-platform/warehouse/boot/src/main/java/com/_6core/platform/warehouse/boot/WarehouseServiceStarter.java
+++ 3-platform/warehouse/boot/src/main/java/com/_6core/platform/warehouse/boot/WarehouseServiceStarter.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com._6core.platform")
+@ComponentScan("com._6core.platform.warehouse")
 public class WarehouseServiceStarter {
   public static void main(String[] args) {
     SpringApplication.run(WarehouseServiceStarter.class, args);
---
+++ 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/mapper/ImmutableInventory2InventoryEntity.java
@@ -0,0 +1,20 @@
+package com._6core.platform.warehouseinfra.mapper;
+
+import com._6core.lib.java.domain.model.warehouse.InventoryV01;
+import com._6core.lib.java.domain.model.warehouse.immutable.ImmutableInventoryV01Impl;
+import com._6core.platform.warehouseinfra.adapter.driven.persistence.entity.InventoryEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = {ImmutableProduct2ProductEntity.class, ImmutableWarehouse2WarehouseEntity.class})
+public interface ImmutableInventory2InventoryEntity {
+  ImmutableInventory2InventoryEntity INSTANCE =
+      Mappers.getMapper(ImmutableInventory2InventoryEntity.class);
+
+  @Mapping(target = "inventoryId", source = "id")
+  ImmutableInventoryV01Impl map2Domain(InventoryEntity entity);
+
+  @Mapping(target = "id", source = "inventoryId")
+  InventoryEntity map2Entity(InventoryV01 domainInventory);
+}
