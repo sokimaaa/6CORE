0c514f8896c9606c6d768ca2668302f7aca70404
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-05-07 11:22:30 +0300

    [6CORE-102] fix checkstyle

--- 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/persistent/PutOnHoldWarehouseReactiveAdapter.java
+++ 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/persistent/PutOnHoldWarehouseReactiveAdapter.java
@@ -9,22 +9,23 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class PutOnHoldWarehouseReactiveAdapter implements InventoryPersistentPort {
-    private final InventoryReactiveRepository inventoryRepository;
+  private final InventoryReactiveRepository inventoryRepository;
 
-    public PutOnHoldWarehouseReactiveAdapter(InventoryReactiveRepository inventoryRepository) {
-        this.inventoryRepository = inventoryRepository;
-    }
+  public PutOnHoldWarehouseReactiveAdapter(InventoryReactiveRepository inventoryRepository) {
+    this.inventoryRepository = inventoryRepository;
+  }
 
-    @Override
-    public Mono<InventoryV01> findByProductId(String productId) {
-        return inventoryRepository.findByProductId(productId)
-                .map(ImmutableInventory2InventoryEntity.INSTANCE::map2Domain);
-    }
+  @Override
+  public Mono<InventoryV01> findByProductId(String productId) {
+    return inventoryRepository
+        .findByProductId(productId)
+        .map(ImmutableInventory2InventoryEntity.INSTANCE::map2Domain);
+  }
 
-    @Override
-    public Mono<InventoryV01> update(InventoryV01 inventory) {
-        return inventoryRepository
-                .save(ImmutableInventory2InventoryEntity.INSTANCE.map2Entity(inventory))
-                .map(ImmutableInventory2InventoryEntity.INSTANCE::map2Domain);
-    }
+  @Override
+  public Mono<InventoryV01> update(InventoryV01 inventory) {
+    return inventoryRepository
+        .save(ImmutableInventory2InventoryEntity.INSTANCE.map2Entity(inventory))
+        .map(ImmutableInventory2InventoryEntity.INSTANCE::map2Domain);
+  }
 }
