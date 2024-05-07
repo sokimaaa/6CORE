e16e8f8f4f5c555e267e73fbb7fa130935c77c29
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-05-07 20:50:11 +0200

    [6CORE-101] corrected query

--- 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/driven/persistent/repository/ProductsReactiveRepository.java
+++ 3-platform/warehouse/infra/src/main/java/com/_6core/platform/warehouseinfra/adapter/driven/persistent/repository/ProductsReactiveRepository.java
@@ -4,11 +4,10 @@ import com._6core.platform.warehouseinfra.adapter.driven.persistent.entity.Produ
 import java.util.List;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 public interface ProductsReactiveRepository extends R2dbcRepository<ProductEntity, String> {
 
-  @Query("SELECT p FROM ProductEntity p WHERE p.id IN (:ids)")
+  @Query("SELECT p FROM ProductEntity p WHERE p.id IN (?1)")
   Flux<ProductEntity> getProductsByIds(List<String> ids);
 }
